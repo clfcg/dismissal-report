f8dd6fa2ce2829e746de4cbedd355696a5fc109d
clfcg <leontev_dma@mail.ru>
2023-01-26 15:39:22 +0300

    feat: added select from dismissal table and edit config file
    feat: added select from dismissal table and edit config file

--- dismissal_report/dismissal.py
+++ dismissal_report/dismissal.py
@@ -14,31 +14,48 @@ def connection_to_db():
         config_path = 'dismissal_report/config/db_conf.ini'
         config = ConfigParser()
         config.read(config_path)
-        connection = pg.connect(host=config["db_login"]["host"],
-                                port=config["db_login"]["port"],
-                                database=config["db_login"]["name"],
-                                user=config["db_login"]["user"],
-                                password=config["db_login"]["password"])
+        connection = pg.connect(host=config["db_options"]["host"],
+                                port=config["db_options"]["port"],
+                                database=config["db_options"]["name"],
+                                user=config["db_options"]["user"],
+                                password=config["db_options"]["password"])
 
         cursor = connection.cursor()
-        print(unload_date, "\tСоединение установлено.")
-        print(config["db_login"]["table_name"])
+        print(f"{unload_date}\t>\tСоединение установлено.")
 
-        q1 = """
-            create table if not exists {0} (k varchar(50));
-            """.format(config["db_login"]["table_name"])
+        #Проверка и создание таблицы с уволенными сотрудниками.
+        query_check_table = """
+            create table if not exists {0} (obl varchar(10) NOT NULL,
+                                            oiv varchar(255) NOT NULL,
+                                            org varchar(255) NOT NULL,
+                                            inn varchar(20) NOT NULL,
+                                            employee varchar(100) NULL,
+                                            subdivision varchar(255) NULL,
+                                            post varchar(255) NULL,
+                                            date_dismissal date NULL,
+                                            year_month varchar(20) NULL);
+            """.format(config["db_options"]["table_name"])
+        cursor.execute(query_check_table)
 
-        print(q1)
-        cursor.execute(q1)
-        connection.commit()
-
+        #Выборка всех записей из таблицы.
+        query_select_all_records = """
+            SELECT * 
+            FROM {0}
+            ORDER BY oiv, org, employee
+            """.format(config["db_options"]["table_name"])
+        cursor.execute(query_select_all_records)
+        records = cursor.fetchall()
+        print(f"{unload_date}\t>\tДанные получены из БД.")
+        return records
     except (Exception, Error) as error:
-        print("Ошибка подключения", error)
-
-    else:
-        cursor.close()
-        connection.close()
-        print("Подключение к БД закрыто.")
+        connection = None
+        print(f"{unload_date}\t>\tОшибка подключения\n{error}")
+    finally:
+        if connection:
+            connection.commit()
+            cursor.close()
+            connection.close()
+            print(f"{unload_date}\t>\tПодключение к БД закрыто.")
 
 if __name__ == "__main__":
     main()
